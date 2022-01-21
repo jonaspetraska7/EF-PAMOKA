52a5ca68b2dae5b43f743e289eb3cf89f049bf0b
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2022-01-21 21:36:00 +0200

    frontend pradzia

---
+++ frontend/projektas/src/app/page/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  forma = new FormGroup({
+    vardas : new FormControl('', Validators.required),
+    slaptazodis : new FormControl('', Validators.required)
+  })
+
+  constructor(private apiService : ApiService) { }
+
+  ngOnInit(): void {
+  }
+
+  prisijungti(){
+    console.log(this.forma.value)
+    this.apiService.prisijungti(this.forma.value)
+    // https://localhost:44321/login
+  }
+
+
+
+}
